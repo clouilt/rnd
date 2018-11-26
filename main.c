3e7bdb06b82bf76d56002c83fa322dd513f7af08
clouilt <clouilt@gmail.com>
2018-11-26 16:55:48 +0800

    .

--- main.c
+++ main.c
@@ -14,17 +14,14 @@ void init(int seed){
 			r[i] += 2147483647;
 		}
 	}
-	for (i=31; i<34; i++) {
-		r[i%R] = r[(i+R-31)%R];
-	}
 	for (i=34; i<344; i++) {
-		r[i%R] = r[(i+R-31)%R] + r[(i+R-3)%R];
+		r[i%R] += r[(i+R-3)%R];
 	}
 	randomcnt = i%R;
 }
 
 unsigned int random(){
-	r[randomcnt%R] =  r[(randomcnt+R-31)%R] + r[(randomcnt+R-3)%R];
+	r[randomcnt%R] += r[(randomcnt+R-3)%R];
 	return ((unsigned int)r[randomcnt++ %R]) >> 1; 
 }
 

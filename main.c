464fe3729e7322ddb79a7fff69f3060094dd261e
clouilt <clouilt@gmail.com>
2018-12-03 18:27:35 +0800

    add randomprev

--- main.c
+++ main.c
@@ -2,34 +2,55 @@
 #define R 31
 #define defaultseed 1
 
-int r[R];
-int randomcnt;
+unsigned r[R];
+unsigned randomcnt;
 
-void init(int seed){
-	int i;
+unsigned f(unsigned a){
+	return a % R;
+}
+
+void init(unsigned seed){
+	unsigned i;
 	r[0] = seed;
 	for (i=1; i<31; i++) {
-		r[i] = (16807LL * r[i-1]) % 2147483647;
-		if (r[i] < 0) {
-			r[i] += 2147483647;
-		}
+		r[f(i)] = (16807LL * r[f(i-1)]) % 2147483647;
+	}
+	r[f(31)] = seed;
+	for (i=32; i<34; i++) {
+		r[f(i)] = (16807LL * r[f(i-1)]) % 2147483647;
 	}
 	for (i=34; i<344; i++) {
-		r[i%R] += r[(i+R-3)%R];
+		r[f(i)] += r[f(i-3)];
 	}
-	randomcnt = i%R;
+	randomcnt = i;
 }
 
 unsigned int random(){
-	r[randomcnt%R] += r[(randomcnt+R-3)%R];
-	return ((unsigned int)r[randomcnt++ %R]) >> 1; 
+	r[f(randomcnt)] += r[f(randomcnt-3)];
+	return ((unsigned int)r[f(randomcnt++)]) >> 1; 
+}
+
+unsigned int randomprev(){
+	if (randomcnt <= 344) 
+		return 0xffffffff;
+	unsigned tmp = r[f(--randomcnt)];
+	r[f(randomcnt)] -= r[f(randomcnt-3)];
+	return ((unsigned int)tmp) >> 1; 
 }
 
 int main() {
   int i;
   init(defaultseed);
 
-  for (i=0; i<30; i++) {
+  for (i=0; i<60; i++) {
     printf("%d\n", random());
   }
+
+  printf("---\n");
+
+  for (i=0; i<61; i++) {
+    printf("%d\n", randomprev());
+  }
+
+  return 0;
 }
